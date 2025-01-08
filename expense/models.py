856f43166a20e054e436d51a433df69b449e97b1
Salohiddin Usmonaliyev <usmonaliyevsalohiddin@outlook.com>
2025-01-08 13:19:16 +0500

    models.py

--- expense/models.py
+++ expense/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+class Value(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Values"
+
+class Expense(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    value = models.ForeignKey(Value, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.amount} {self.category.name} | {self.user}"
