7133f450a3a9151e04d5909374748ead7f5ad663
Salohiddin Usmonaliyev <usmonaliyevsalohiddin@outlook.com>
2025-02-02 14:12:31 +0500

    update

--- expense/views.py
+++ expense/views.py
@@ -5,3 +5,12 @@ from django.views.generic import TemplateView
 # Create your views here.
 class DashboardTemplateView(TemplateView):
     template_name = 'dashboard.html'
+
+class BudgetsTemplateView(TemplateView):
+    template_name = 'budgets.html'
+
+class AnalyticsTemplateView(TemplateView):
+    template_name = 'analytics.html'
+
+class TransactionsTemplateView(TemplateView):
+    template_name = 'transactions.html'
